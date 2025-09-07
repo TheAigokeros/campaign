3032faa102a29cd67f60d2100cbd25c1c5e022fc
poramet <aigokeros.flok@gmail.com>
2025-09-07 19:27:19 +0700

    initialize project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+ 
+export default function Page() {
+  return <div>
+        <Image src="/profile.png" alt="Profile" width={100} height={100} />
+        <h1> test </h1>        
+        <h1 className="text-2xl font-bol outline-dashed outline-offset-2">Welcome to Next.js and tailwinds!</h1>
+
+    </div>
+}
---
+++ src/app/somemore/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Image from 'next/image'
+import StyledButton from '@/components/StyledButton'
+ 
+export default function Somemore() {
+  return <div>       
+        <h1> other pages </h1>       
+        <StyledButton > approve </StyledButton>
+    </div>
+}
